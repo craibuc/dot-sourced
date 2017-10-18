0bec61b01f69a2f772b2e540ae8d5ba0452ab1c4
Craig Buchanan <craig.buchanan@cogniza.com>
2017-10-18 13:50:46 -0700

    adding Slice-Array

---
+++ Slice-Array.Tests.ps1
@@ -0,0 +1,19 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Slice-Array" {
+
+    $ids = @(0,1,2,3,4,5,6,7,8,9)
+
+	Context "Default" {
+
+        $actual = $ids | Slice-Array -Size 3 
+
+	    It "converts an array of 10 elements to 4 arrays of 3 or fewer elements" {
+	        $actual.length | Should Be 4
+	    }
+
+	}
+
+}
---
+++ Slice-Array.ps1
@@ -0,0 +1,40 @@
+﻿
+<#
+.SYNOPSIS
+Converts an array into other arrays of the specified size
+
+.PARAMETER Item
+
+.PARAMETER Size
+
+.EXAMPLE
+Convert an array of 10 elements to 4 arrays of 3 or fewer elements
+
+PS> @(0,1,2,3,4,5,6,7,8,9) | Slice-Array -Size 3 | % { "IDs: $($_ -Join ",")" }
+IDs: 0,1,2
+IDs: 3,4,5
+IDs: 6,7,8
+IDs: 9
+
+#>
+
+function Slice-Array
+{
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$True)]
+        [String[]]$Item,
+        [int]$Size = 10
+    )
+    BEGIN { $Items=@()}
+    PROCESS {
+        foreach ($i in $Item ) { $Items += $i }
+    }
+    END {
+        0..[math]::Floor($Items.count/$Size) | % { 
+            $x, $Items = $Items[0..($Size-1)], $Items[$Size..$Items.Length]; ,$x
+        } 
+    }
+}
+
