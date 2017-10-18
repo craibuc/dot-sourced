6623b3346d62ee9fbc633b1e052278d7adaa4934
Craig Buchanan <craig.buchanan@cogniza.com>
2017-10-18 13:50:16 -0700

    correcting behavior; adding tests

---
+++ Get-ADUser.Tests.ps1
@@ -0,0 +1,29 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-ADUser" {
+
+    $account = ''
+
+	Context "Valid account is supplied" {
+
+	    It "returns a PsCustomObject that contains a property matching the account" {
+	        $actual = Get-ADUser $account
+
+            $actual.GetType() | Should Be System.Management.Automation.PsCustomObject
+            $actual.samaccountname | Should Be $account
+	    }
+
+	}
+
+    Context "Invalid account is supplied" {
+
+        It "returns a null" {
+            $actual = Get-ADUser 'foobarbaz'
+            $actual | Should BeNullOrEmpty
+        }
+
+    }
+
+}
---
+++ Get-ADUser.ps1
@@ -0,0 +1,88 @@
+﻿<#
+.SYNOPSIS
+Query the Active Directory service by account, first name, or last name.
+
+.PARAMETER account
+
+.PARAMETER account
+
+.PARAMETER account
+
+.EXAMPLE
+Query by account:
+
+PS> Get-ADUser s0123456
+
+Name            : Jane Doe
+givenname       : Jane
+sn              : Doe
+SAMAccountName  : s0123456
+mails           :
+telephonenumber :
+title           :
+department      :
+homedirectory   :
+
+.EXAMPLE
+Query by first name:
+
+PS> Get-ADUser -FirstName Jane
+
+Name            : Jane Doe
+givenname       : Jane
+sn              : Doe
+SAMAccountName  : s0123456
+mails           :
+telephonenumber :
+title           :
+department      :
+homedirectory   :
+
+.EXAMPLE
+Query by last name:
+
+PS> Get-ADUser -LastName Doe
+
+Name            : Jane Doe
+givenname       : Jane
+sn              : Doe
+SAMAccountName  : s0123456
+mails           :
+telephonenumber :
+title           :
+department      :
+homedirectory   :
+
+#>
+function Get-ADUser {
+
+	param(
+		[string]$Account,
+		[string]$FirstName,
+		[string]$LastName
+	)
+
+	$f=@()
+	$f += if ( $Account ) { "(samaccountname=$Account)" }
+	$f += if ( $FirstName ) { "(givenname=$FirstName)" }
+	$f += if ( $LastName ) { "(sn=$LastName)" }
+
+	$filter = "(&(objectCategory=person)(objectClass=user)$( $f -Join ''))"
+	Write-Debug "Filter: $filter"
+
+    $results = ([ADSISearcher]$filter).FindAll()
+
+    if ($results.properties) {
+
+        $properties=@{}
+
+        # convert ResultPropertyCollection to PsCustomObject
+        $results.Properties.GetEnumerator() | % { 
+            $properties[$_.Name]= if ($_.Value.Count -gt 1) { $_.Value.ForEach({$_}) -Join ","} else { $_.Value.Item(0) }
+        }
+
+        [PsCustomObject]$properties
+
+    }
+
+}
